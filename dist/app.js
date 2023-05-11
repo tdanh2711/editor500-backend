ffc8d84eaf2f79575f698a4ec307f4efbc3a719f
tdanh2711 <tdanh2711@gmail.com>
2023-05-12 01:07:42 +0700

    Update build

--- dist/app.js
+++ dist/app.js
@@ -13,8 +13,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const aws_1 = __importDefault(require("./aws"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.get('/upload-url', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { fileName, fileType } = req.query;
     if (!fileName) {
